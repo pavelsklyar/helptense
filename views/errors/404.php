1b6d1c6dcef288198c8769af21a0c732e8bb7c48
Arina Kulikova <rina.runet@gmail.com>
2019-11-10 19:57:06 +0300

    errors

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,1 +1,8 @@
-page not found!
+<div class="content">
+
+<div align="center" class="signature">
+    <p class="sign-p-not-up">Ошибка 404</a>
+    <p class="sign-p-not-down">К сожалению, страница не найдена</p>
+</div>
+
+</div>
--- views/errors/verb.php
+++ views/errors/verb.php
@@ -1,1 +1,16 @@
-такой глагол не найден!
+<div class="content">
+    
+    <form action="" method="get">
+            <input name="s" placeholder="Введите глагол..." type="search">
+            <button type="submit"><img class="search" src="img/search_w.png"></button>
+    </form>
+
+    <div align="center" class="sign-verb">
+            <p class="sign-p-not-up">глагол не найден</a>
+            <p class="sign-p-not-down">Возможно, вы имели в виду:</p>
+            <a href="" class="verb-found">Verb</a>
+            <a href="" class="verb-found">Verb</a>
+            <a href="" class="verb-found">Verb</a>
+        </div>
+    
+    </div>
