e35d002c7fbf95e02d0e68cf2ca3798d4ec4d96e
Pavel Sklyar <ozpavel@yandex.ru>
2019-11-10 20:48:49 +0300

    local fix

--- views/errors/verb.php
+++ views/errors/verb.php
@@ -1,16 +1,16 @@
 <div class="content">
     
     <form action="" method="get">
-            <input name="s" placeholder="Введите глагол..." type="search">
-            <button type="submit"><img class="search" src="img/search_w.png"></button>
+        <input name="verb" placeholder="Введите глагол..." type="search">
+        <button type="submit"><img class="search" src="img/search_w.png"></button>
     </form>
 
     <div align="center" class="sign-verb">
-            <p class="sign-p-not-up">глагол не найден</a>
-            <p class="sign-p-not-down">Возможно, вы имели в виду:</p>
-            <a href="" class="verb-found">Verb</a>
-            <a href="" class="verb-found">Verb</a>
-            <a href="" class="verb-found">Verb</a>
-        </div>
-    
+        <p class="sign-p-not-up">глагол не найден</p>
+        <p class="sign-p-not-down">Возможно, вы имели в виду:</p>
+        <a href="" class="verb-found">Verb</a>
+        <a href="" class="verb-found">Verb</a>
+        <a href="" class="verb-found">Verb</a>
     </div>
+
+</div>
