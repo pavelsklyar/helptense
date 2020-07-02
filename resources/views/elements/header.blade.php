dccf232f3fd7264120733de7f1d274519db3f908
Arina Kulikova <rina.runet@gmail.com>
2020-07-02 16:22:25 +0300

    новый дизайн

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -1,9 +1,17 @@
 <div class="main">
-    <a href="{{ route("home") }}"><img class="logo" src="/img/Logo.png"></a>
+    <a href="{{ route("home") }}"> 
+        <div class="logo">
+            <h1 class="color_white">Help</h1>
+            <h1 class="color_red">Tense</h1>
+        </div>
+    </a>
     <div class="menu">
         <ul class="menu-ul">
             <li class="menu-desktop-li">
-                <a href="{{ route("home") }}" class="menu-a">Главная</a>
+                <a href="{{ route("home") }}" class="menu-a">Неправильные глаголы</a>
+            </li>
+            <li class="menu-desktop-li">
+                <a href="" class="menu-a">Фразовые глаголы</a>
             </li>
             <li class="menu-desktop-li">
                 <a href="{{ route("tense") }}" class="menu-a">Таблица времен</a>
@@ -11,7 +19,7 @@
             <li class="menu-desktop-li">
                 <a href="{{ route("grammar") }}" class="menu-a">Основы грамматики</a>
             </li>
-            <li class="menu-mobile">
+            <!-- <li class="menu-mobile">
                 <a href="" class="menu-a">Меню</a>
                 <ul class="dropdown">
                     <li class="menu-mobile-li">
@@ -24,7 +32,7 @@
                         <a href="{{ route("grammar") }}" class="dropdown-a">Основы грамматики</a>
                     </li>
                 </ul>
-            </li>
+            </li> -->
         </ul>
     </div>
 </div>
